53043ebefcd3728f72614ad39f757d561c1191fe
name1e5s <name1e5s@qq.com>
2021-11-28 15:53:57 +0800

    add third_party library for jwt

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <jwt-cpp/jwt.h>
 #include <db/db.h>
 
 int main()
 {
     db::init_storage();
     std::cout << "Hello, World!" << std::endl;
+    std::string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+    auto decoded = jwt::decode(token);
+
+    for(auto& e : decoded.get_payload_claims())
+        std::cout << e.first << " = " << e.second << std::endl;
     return 0;
 }
